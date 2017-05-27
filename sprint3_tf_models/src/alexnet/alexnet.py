840e0ae7c12f92d0034f35d6be2544d298fa088a
Niklas H <niklas.haas@student.kit.edu>
2017-05-27 19:32:07 +0200

    First draft of code structure for alexnet

--- sprint3_tf_models/src/alexnet/alexnet.py
+++ sprint3_tf_models/src/alexnet/alexnet.py
@@ -10,46 +10,71 @@ from __future__ import (absolute_import, division,
                         print_function, unicode_literals)
 from builtins import *
 
-# create a class: data Loader
+import tensorflow.contrib.slim as slim
 
-## method: download data and specify destination
+class DataLoader:
+    # create a class: data Loader
 
-## method: get training batch
+    ## method: download data and specify destination
 
-## method: get test batch
+    ## method: get training batch
 
-
-# create a class AlexNet
-
-## place default values here for
-
-## define the argument parser here and hand it to the executing trainNet file. Arguments may differ largely between network types
-
-## method: graph part: inference
-
-## method: graph part: loss
-
-## method: graph part: training
-
-## method: graph part: evaluation / accuracy / other evaluation metrics
-
-## method: build graph (use the methods above)
-
-## method: create summaries (maybe solve this with an decorator!
-
-## method: create embedding
-
-## method: export checkpoints
+    ## method: get test batch
 
 
-# create class TrainNet: should be agnostic to the neural net. dunno if this is possible.
+class AlexNet:
+    # create a class AlexNet
 
-## import the model
+    ## place default values here centrally
 
-## essentially a main function with an iterator over a batch
+    def inference(self, input):
+        ## method: graph part: inference
+        pass
 
-## specify batch_size, learning rate, no of epochs, optimizer
+    def loss(self):
+        ## method: graph part: loss
+        pass
 
-## questionable: specify loss function, activation function, initializations?
+    def training(self):
+        ## method: graph part: training
+        pass
+
+    def metrics(self):
+        ## method: graph part: evaluation / accuracy / other evaluation metrics than the loss (which is optimized)
+        pass
+
+    def build_graph(self):
+        ## method: build graph (use the methods above)
+        pass
+
+    def arg_parse(self):
+        ## define the argument parser here and hand it to the executing trainNet file.
+        #  Arguments may differ largely between network types.
+        # put AlexNet is defined completely by  special parameter setting
+        pass
+
+    def create_summaries(self):
+        ## method: create summaries (maybe solve this with an decorator!
+        pass
+
+    def create_embeddings(self):
+        ## method: create embedding
+        pass
+
+    def create_checkpoint(self):
+        ## method: export checkpoints: where to put this?
+        pass
+
+
+class TrainNet:
+    # create class TrainNet: should be agnostic to the neural net. dunno if this is possible.
+
+    ## import the model
+
+    ## essentially a main function with an iterator over a batch
+
+    ## specify batch_size, learning rate, no of epochs, optimizer
+
+    ## questionable: specify loss function, activation function, initializations?
 
 
